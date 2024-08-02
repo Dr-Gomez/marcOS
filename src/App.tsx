58f248ddd213549c2b160ecacbe9dfe7c5e3be86
Dr-Gomez <gomez.contact.mgz@gmail.com>
2024-08-02 22:04:58 +0100

    I got the project running!
    Added a wallpaper and started the entire repository.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import wallpaper from './assets/wallpaper1.png';
+
+function App() {
+  return (
+    <>
+      <img src={wallpaper} className="wallpaper" id="wallpaper" />
+    </>
+  );
+}
+
+export default App;
