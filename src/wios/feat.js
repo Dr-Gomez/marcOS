a0190dcf9e40f35a3e7c7e37b1ae8e72c95aca35
Marco A. Gomez <gomez.contact@proton.me>
2025-08-02 14:55:34 +0100

    Chapter 3: The Sentinel of Features

---
+++ src/wios/feat.js
@@ -0,0 +1,69 @@
+(function () {
+  var API;
+
+  function setAPI(possibleApi) {
+    if (possibleApi) {
+      API = possibleApi;
+    } else {
+      API = window;
+    }
+  }
+
+  function isFeatureAbsent(featureStr) {
+    var currentAPI = API;
+    var parts = featureStr.split(".");
+
+    var joinedParts = "";
+
+    for (var depth = 0; depth < parts.length; depth++) {
+      var nextPart = parts[depth];
+      joinedParts += nextPart;
+      var path = currentAPI[nextPart];
+      if (typeof path == "undefined") {
+        return true;
+      } else {
+        currentAPI = path;
+      }
+    }
+
+    return false;
+  }
+
+  var feat = {};
+
+  feat.checkFeatures = function (features, possibleApi) {
+    var absentFeatures = [];
+
+    setAPI(possibleApi);
+
+    for (var featureIndex = 0; featureIndex < features.length; featureIndex++) {
+      var feature = features[featureIndex];
+
+      var absent = isFeatureAbsent(feature);
+
+      if (absent) {
+        var latestAbsence = absentFeatures.length;
+        absentFeatures[latestAbsence] = feature;
+      }
+    }
+
+    return absentFeatures;
+  };
+
+  feat.hasFeature = function (feature, possibleApi) {
+    setAPI(possibleApi);
+    return !isFeatureAbsent(feature);
+  };
+
+  feat.hasFeatures = function (features, possibleApi) {
+    for (var featureIndex = 0; featureIndex < features.length; featureIndex++) {
+      if (!feat.hasFeature(features[featureIndex], possibleApi)) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  marcOS.feat = feat;
+})();
