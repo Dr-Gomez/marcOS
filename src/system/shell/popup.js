53a007597c1f587ab795b9b847975d72e55a4fcf
Marco A. Gomez <gomez.contact@proton.me>
2025-08-03 01:02:36 +0100

    Chapter 8: The Pride of Comfort

--- src/system/shell/popup.js
+++ src/system/shell/popup.js
@@ -1,6 +1,7 @@
 (function () {
   var DISTANCE = 4;
   var BUTTON_WIDTH = 18;
+  var SNAP_ZONE = 64;
 
   var popup = {};
 
@@ -50,10 +51,13 @@
         frame.style.left = (left - offsetX).toString() + "px";
         frame.style.top = (top - offsetY).toString() + "px";
 
-        if (left < 10) {
+        if (left < 100) {
           leftSnap = true;
           rightSnap = false;
-        } else if (left > screen.width - 10) {
+        } else if (
+          left >
+          (window.innerWidth || screen.availWidth || screen.width) - SNAP_ZONE
+        ) {
           leftSnap = false;
           rightSnap = true;
         } else {
@@ -61,10 +65,14 @@
           rightSnap = false;
         }
 
-        if (top < 10) {
+        if (top < 100) {
           topSnap = true;
           bottomSnap = false;
-        } else if (top > screen.height - 10) {
+        } else if (
+          top >
+          (window.innerHeight || screen.availHeight || screen.height) -
+            SNAP_ZONE
+        ) {
           topSnap = false;
           bottomSnap = true;
         } else {
@@ -76,6 +84,64 @@
 
     document.onmouseup = function () {
       isDragging = false;
+
+      if (topSnap && leftSnap) {
+        frame.style.left = "0";
+        frame.style.right = "";
+        frame.style.top = "0";
+        frame.style.bottom = "";
+        frame.style.width = "50%";
+        frame.style.height = "50%";
+      } else if (topSnap && rightSnap) {
+        frame.style.right = "0";
+        frame.style.left = "";
+        frame.style.top = "0";
+        frame.style.bottom = "";
+        frame.style.width = "50%";
+        frame.style.height = "50%";
+      } else if (bottomSnap && rightSnap) {
+        frame.style.right = "0";
+        frame.style.left = "";
+        frame.style.bottom = "0";
+        frame.style.top = "";
+        frame.style.width = "50%";
+        frame.style.height = "50%";
+      } else if (bottomSnap && leftSnap) {
+        frame.style.left = "0";
+        frame.style.right = "";
+        frame.style.bottom = "0";
+        frame.style.top = "";
+        frame.style.width = "50%";
+        frame.style.height = "50%";
+      } else if (topSnap) {
+        frame.style.left = "0";
+        frame.style.right = "";
+        frame.style.top = "0";
+        frame.style.bottom = "";
+        frame.style.width = "100%";
+        frame.style.height = "50%";
+      } else if (rightSnap) {
+        frame.style.right = "0";
+        frame.style.left = "";
+        frame.style.top = "0";
+        frame.style.bottom = "";
+        frame.style.width = "50%";
+        frame.style.height = "100%";
+      } else if (bottomSnap) {
+        frame.style.left = "0";
+        frame.style.right = "";
+        frame.style.bottom = "0";
+        frame.style.top = "";
+        frame.style.width = "100%";
+        frame.style.height = "50%";
+      } else if (leftSnap) {
+        frame.style.left = "0";
+        frame.style.right = "";
+        frame.style.top = "0";
+        frame.style.bottom = "";
+        frame.style.width = "50%";
+        frame.style.height = "100%";
+      }
     };
 
     var title = document.createElement("span");
@@ -120,4 +186,6 @@
   };
 
   marcOS.shell.popup = popup;
+
+  monitor.appendChild(popup.create({ title: "Do" }));
 })();
